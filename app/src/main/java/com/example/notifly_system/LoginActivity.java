ab36daf6b47ae0fae7372edb72044d8e9e1d322a
abelwho <nabilisah18@gmail.com>
2026-04-08 00:54:20 +0800

    Rename class login_activity to LoginActivity

--- app/src/main/java/com/example/notifly_system/LoginActivity.java
+++ app/src/main/java/com/example/notifly_system/LoginActivity.java
@@ -1,6 +1,6 @@
 package com.example.notifly_system;
 
-public class login_activity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
