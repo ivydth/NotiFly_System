9239ed2b38954dfc9668c822ac3a11e04c4fbf3a
abelwho <nabilisah18@gmail.com>
2026-04-08 00:54:02 +0800

    Rename LoginActivity to login_activity and extend AppCompatActivity

--- app/src/main/java/com/example/notifly_system/LoginActivity.java
+++ app/src/main/java/com/example/notifly_system/LoginActivity.java
@@ -1,4 +1,9 @@
 package com.example.notifly_system;
 
-public class LoginActivity {
+public class login_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); 
+    }
 }
