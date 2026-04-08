79d9b882638b43d61f2132de0986cf4347bbe830
ivydth <ivydth@gmail.com>
2026-04-08 17:24:57 +0800

    Add google-services(1).json

--- app/src/main/java/com/example/notifly_system/RegisterActivity.java
+++ app/src/main/java/com/example/notifly_system/RegisterActivity.java
@@ -1,5 +1,9 @@
 package com.example.notifly_system;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
