9239dab96933cfea3617f4f2fe7c7f854a77e698
abelwho <nabilisah18@gmail.com>
2026-04-08 19:07:02 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/notifly_system/MainActivity.java
+++ app/src/main/java/com/example/notifly_system/MainActivity.java
@@ -3,32 +3,43 @@ package com.example.notifly_system;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogin;
     private Button btnCreateAccount;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         btnLogin = findViewById(R.id.btnLogin);
         btnCreateAccount = findViewById(R.id.btnCreateAccount);
 
         // Goes to Login screen
         btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         });
 
         // Goes to Register screen
         btnCreateAccount.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // if already logged in, skip welcome screen and go to dashboard
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(this, DashboardActivity.class));
+            finish();
+        }
     }
 }
