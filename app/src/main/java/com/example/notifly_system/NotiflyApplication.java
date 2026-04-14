78b184b461599a3f812a5ad302765ec33b9953c3
abelwho <nabilisah18@gmail.com>
2026-04-14 17:39:43 +0800

    Update NotiflyApplication.java

--- app/src/main/java/com/example/notifly_system/NotiflyApplication.java
+++ app/src/main/java/com/example/notifly_system/NotiflyApplication.java
@@ -7,6 +7,10 @@ public class NotiflyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Create notification channels once at app startup
+        NotiflyMessagingService.createChannels(this);
+
         // Start listening to Firebase /notifications as soon as app launches
         FirebaseNotifSyncService.getInstance().startListening();
     }
