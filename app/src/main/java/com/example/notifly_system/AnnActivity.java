8da3896c09d1332f03a2ce6b7ef95a4bd366eda7
abelwho <nabilisah18@gmail.com>
2026-04-15 00:49:20 +0800

    Change layout resource for AnnActivity

--- app/src/main/java/com/example/notifly_system/AnnActivity.java
+++ app/src/main/java/com/example/notifly_system/AnnActivity.java
@@ -51,7 +51,7 @@ public class AnnActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_star);
+        setContentView(R.layout.announcements_activity);
 
         initViews();
         initFirebase();
